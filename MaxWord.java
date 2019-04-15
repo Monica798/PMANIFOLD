1f5e3957ecd8ad6a3ce787a9cae4875a693bc159
Monica798 <monicamuskudi@gmail.com>
2019-04-15 22:52:36 +0530

    Add files via upload

---
+++ MaxWord.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pmf;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class MaxWord {
+ 
+    private int currentMaxCount = 0;
+    private List<String> lines = new ArrayList<String>();
+     
+    public void readMaxLineCount(String fileName){
+ 
+        FileInputStream fis = null;
+        DataInputStream dis = null;
+        BufferedReader br = null;
+         
+        try {
+            fis = new FileInputStream(fileName);
+            dis = new DataInputStream(fis);
+            br = new BufferedReader(new InputStreamReader(dis));
+            String line = null;
+            while((line = br.readLine()) != null){
+                 
+                int count = (line.split("\\s+")).length;
+                if(count > currentMaxCount){
+                    lines.clear();
+                    lines.add(line);
+                    currentMaxCount = count;
+                } else if(count == currentMaxCount){
+                    lines.add(line);
+                } 
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            try{
+                if(br != null) br.close();
+            }catch(Exception ex){}
+        }
+    }
+ 
+    public int getCurrentMaxCount() {
+        return currentMaxCount;
+    }
+ 
+    public void setCurrentMaxCount(int currentMaxCount) {
+        this.currentMaxCount = currentMaxCount;
+    }
+ 
+    public List<String> getLines() {
+        return lines;
+    }
+ 
+    public void setLines(List<String> lines) {
+        this.lines = lines;
+    }
+    public boolean isFiller(String s){
+        String [] filler={"a","an","the","in","on","by"};
+        boolean test = false; 
+        for (String element : filler) { 
+            if (element.equals(s)) { 
+                test = true; 
+                break; 
+            } 
+        } 
+        return test;
+    }
+     
+    public static void main(String a[]){
+         
+        MaxWord mdc = new MaxWord();
+        mdc.readMaxLineCount("C:\\Users\\DELL\\Desktop\\hi.txt");
+        //System.out.println("Max number of words in a line is: "+mdc.getCurrentMaxCount());
+        System.out.println("Line with max word count: i.e.,longest sentence is:");
+        List<String> lines = mdc.getLines();
+        String [] words = null;
+        String [] syn = null;
+        String [] ant = null;
+        for(String l:lines){
+            System.out.println(l);
+            words=l.split(" ");
+            
+            
+        }
+         
+        for(String l:words){
+            if(!mdc.isFiller(l))
+            {
+                System.out.println(l);
+               ant = antonym.getAntonyms(l);
+               syn = antonym.getSynonyms(l);
+            }
+            break;
+        }
+        System.out.println("Antonyms");
+        for(String l1:ant)
+        {
+            System.out.println(l1);
+        }
+        System.out.println("Synonyms");
+        for(String l1:syn)
+        {
+            System.out.println(l1);
+        }
+    }
+}
